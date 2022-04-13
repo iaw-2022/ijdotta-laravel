de2687c0e7778c4f1fcddb8062637c5703f42feb
Ignacio Dotta <ij.dotta@gmail.com>
2022-04-13 20:39:50 -0300

    Created migrations

---
+++ database/migrations/2022_04_13_225446_create_patients_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id(); // DNI
+            $table->string('name');
+            $table->string('lastname');
+            $table->string('email');
+            $table->string('password');
+            $table->string('health_insurance_company')->nullable();
+            $table->string('health_insurance_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+};
---
+++ database/migrations/2022_04_13_231002_create_appointments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('doctor_id');
+            $table->foreignId('patient_id')->nullable();
+            $table->date('date');
+            $table->time('initial_time');
+            $table->time('end_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
