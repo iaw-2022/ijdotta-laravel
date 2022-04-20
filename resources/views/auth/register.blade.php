3104120837c1af3997b11debdda66d8550e7f02b
Ignacio Dotta <ij.dotta@gmail.com>
2022-04-20 10:22:52 -0300

    Integrated Stisla UI

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,59 +1,82 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('layouts.auth_app')
+@section('title')
+    Register
+@endsection
+@section('content')
+    <div class="card card-primary">
+        <div class="card-header"><h4>Register</h4></div>
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
-
-            <!-- Name -->
-            <div>
-                <x-label for="name" :value="__('Name')" />
-
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
-            </div>
-
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
-            </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-button class="ml-4">
-                    {{ __('Register') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+        <div class="card-body pt-1">
+            <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
+                @csrf
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="first_name">Full Name:</label><span
+                                    class="text-danger">*</span>
+                            <input id="firstName" type="text"
+                                   class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}"
+                                   name="name"
+                                   tabindex="1" placeholder="Enter Full Name" value="{{ old('name') }}"
+                                   autofocus required>
+                            <div class="invalid-feedback">
+                                {{ $errors->first('name') }}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="email">Email:</label><span
+                                    class="text-danger">*</span>
+                            <input id="email" type="email"
+                                   class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
+                                   placeholder="Enter Email address" name="email" tabindex="1"
+                                   value="{{ old('email') }}"
+                                   required autofocus>
+                            <div class="invalid-feedback">
+                                {{ $errors->first('email') }}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="password" class="control-label">Password
+                                :</label><span
+                                    class="text-danger">*</span>
+                            <input id="password" type="password"
+                                   class="form-control{{ $errors->has('password') ? ' is-invalid': '' }}"
+                                   placeholder="Set account password" name="password" tabindex="2" required>
+                            <div class="invalid-feedback">
+                                {{ $errors->first('password') }}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="password_confirmation"
+                                   class="control-label">Confirm Password:</label><span
+                                    class="text-danger">*</span>
+                            <input id="password_confirmation" type="password" placeholder="Confirm account password"
+                                   class="form-control{{ $errors->has('password_confirmation') ? ' is-invalid': '' }}"
+                                   name="password_confirmation" tabindex="2">
+                            <div class="invalid-feedback">
+                                {{ $errors->first('password_confirmation') }}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12 mt-4">
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                                Register
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="mt-5 text-muted text-center">
+        Already have an account ? <a
+                href="{{ route('login') }}">SignIn</a>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,7 @@ Route::get('/main', function() {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
