10df80212d58d74f1aae266ca0e3e758867ebe8d
Ignacio Dotta <ijdotta@gmail.com>
2022-04-28 17:27:51 -0300

    route '/' now redirects to /dashboard

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ use Illuminate\Support\Facades\Auth;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('dashboard');
 });
 
 Route::get('/dashboard', function () {
