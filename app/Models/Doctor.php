c1fabfa9f0f72275fd7c27d4abe82b3f70f343f1
Ignacio Dotta <ijdotta@gmail.com>
2022-04-30 15:29:48 -0300

    defined email accesor through user

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -4,10 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Doctor extends Model
 {
     use HasFactory;
+
+    protected function email(): Attribute {
+        return Attribute::make(
+            get: fn ($value) => $this->user()->get()->first()->email
+        );
+    }
 
     public function user() {
         return $this->belongsTo(User::class);
