2cfb01d4e464446c548070d9952ea8f616f6bb6f
Ignacio Dotta <ijdotta@gmail.com>
2022-04-30 14:25:38 -0300

    removed unnecessary (and incorrect) col: doctor_id

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('doctor_id')->nullable();
             $table->string('role')->default("guest");
             $table->string('name');
             $table->string('email')->unique();
