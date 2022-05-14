e1c2a8add4f0f77a7cadaf85a3df30624de35e02
Ignacio <ij.dotta@gmail.com>
2022-05-14 14:13:18 -0300

    added defualt guest role for users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Role;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,9 +14,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        $guest_role_id = Role::where('role', 'guest')->pluck('id')->first();
+
+        Schema::create('users', function (Blueprint $table) use($guest_role_id) {
             $table->id();
-            $table->foreignId('role_id')->constrained()->cascadeOnUpdate()->restrictOnDelete();
+            $table->foreignId('role_id')->default($guest_role_id)->constrained()->cascadeOnUpdate()->restrictOnDelete();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
